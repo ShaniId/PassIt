08ff4ea8779ba73a0823ec43488aeea4b59407a9
Ofir <ofirbnaim@gmail.com>
2020-11-09 21:13:09 +0200

    added Know the client menu

---
+++ app/src/main/java/com/example/passit1/ToKnowTheClientMenu.java
@@ -0,0 +1,19 @@
+package com.example.passit1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ToKnowTheClientMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_know_the_client_menu);
+
+
+
+    }
+}
