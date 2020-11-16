d5d2359b15d827a915e0b69d29fba0e0c8c5eca5
ShaniId <shani55555@gmail.com>
2020-11-16 13:28:22 +0200

    added login check

--- app/src/main/java/com/example/passit1/PersonalDetailsActivity.java
+++ app/src/main/java/com/example/passit1/PersonalDetailsActivity.java
@@ -5,28 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class PersonalDetailsActivity extends AppCompatActivity implements View.OnClickListener{
+    FirebaseFirestore db;
+    private static final String TAG = PersonalDetailsActivity.class.getSimpleName();
 
     EditText editTextEmail, editTextPassword1, editTextPassword2, editFullName;
+    String gender = "";
     private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_details);
-
+        db = FirebaseFirestore.getInstance();
+        // Create a new user with a first and last name
 
         //Link to Welcome Back page
         findViewById(R.id.Login).setOnClickListener(new View.OnClickListener() {
@@ -44,6 +56,26 @@ public class PersonalDetailsActivity extends AppCompatActivity implements View.O
         editTextPassword2 = (EditText) findViewById(R.id.ConfirmPassword);
         mAuth = FirebaseAuth.getInstance();
         findViewById(R.id.Login).setOnClickListener(this);
+    }
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.checkBoxFemale:
+                if (checked)
+                   gender = "Female";
+            else
+                gender = "";
+                break;
+            case R.id.checkBoxMale:
+                if (checked)
+                gender = "Male";
+            else
+                gender = "";
+                break;
+        }
     }
 
     private void registerUser(){
@@ -63,10 +95,14 @@ public class PersonalDetailsActivity extends AppCompatActivity implements View.O
             return;
         }
 
-        if (password1.isEmpty()){
+        if (password1.isEmpty() || password2.isEmpty()){
             editTextPassword1.setError("Password is required");
             editTextPassword1.requestFocus();
             return;
+        }
+        if (!password1.equals(password2)){
+            editTextPassword1.setError("Password and confirmed password arn't equal");
+            editTextPassword1.requestFocus();
         }
         if (password1.length() < 6){
             editTextPassword1.setError("Minimum length of password should be 6");
@@ -74,11 +110,33 @@ public class PersonalDetailsActivity extends AppCompatActivity implements View.O
             return;
         }
 
+        Map<String, Object> user = new HashMap<>();
+        user.put("FullName", fullName);
+        user.put("Email", email);
+        user.put("Password", password1);
+        user.put("Gender", gender);
+
         mAuth.createUserWithEmailAndPassword(email, password1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()){
                     Toast.makeText(getApplicationContext(), "User Registered Successful", Toast.LENGTH_SHORT).show();
+                    // Add a new document with a generated ID
+                    db.collection("users")
+                            .add(user)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w(TAG, "Error adding document", e);
+                                }
+                            });
+
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "Email already exist", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/passit1/WelcomeBack.java
+++ app/src/main/java/com/example/passit1/WelcomeBack.java
@@ -1,15 +1,85 @@
 package com.example.passit1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.SignInMethodQueryResult;
 
 public class WelcomeBack extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    EditText Email, Password;
+    private static final String TAG = "EmailPassword";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_back);
+        mAuth = FirebaseAuth.getInstance();
+
+        Email = (EditText) findViewById(R.id.Email);
+        Password = (EditText) findViewById(R.id.WelcomeBackPassword);
+//        signIn(Email.getText().toString(),Password.getText().toString());
+        findViewById(R.id.Login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                signIn(Email.getText().toString(),Password.getText().toString());
+//               Intent intent = new Intent(MainActivity.this, PersonalDetailsActivity.class);
+//               startActivity(intent);
+            }
+        });
 
     }
+    public void signIn(String email, String password) {
+        Log.d(TAG, "email & pass : " + email + " " + password);
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Log.d(TAG, user.getEmail());
+//                    updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+//                    updateUI(null);
+                    // ...
+                }
+
+                // ...
+            }
+        });
+    }
+
+//    public void checkEmail(View v){
+//        auth.fetchSignInMethodsForEmail(Email.getText().toString())
+//                .addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
+//                        boolean check = !task.getResult().getSignInMethods().isEmpty();
+//                        if (!check)
+//                        {
+//                            Toast.makeText(getApplicationContext(), "Email not found",Toast.LENGTH_SHORT).show();
+//                        }
+//                        else
+//                        {
+//                            Toast.makeText(getApplicationContext(),"Email already present", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
+//    }
 }
